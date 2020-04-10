6c71c4660abacad21dcd2208528f59643a56c5af
York Yao <me@yorkyao.com>
2020-04-11 06:59:27 +0800

    test: add class test

---
+++ test/class.js
@@ -0,0 +1,30 @@
+const test = require('ava')
+
+const { testParser } = require('./utils')
+
+const title = 'class'
+
+test(title, (t) => {
+  class A {
+    constructor(value) {
+      this.value = value
+    }
+    parent
+  }
+  class B {
+    constructor(value) {
+      this.value = value
+    }
+    child
+  }
+  const a = new A(1)
+  const b = new B(2)
+  a.parent = b
+  b.child = a
+  const { tokens, ast, result, printResult } = testParser(`a.parent.child.parent.child.parent.value`, t, {
+    context: {
+      a: a
+    }
+  })
+  t.snapshot({ tokens, ast, result, printResult }, { id: title })
+})
